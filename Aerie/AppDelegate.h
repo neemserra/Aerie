eb975810a9b1273c73b261c18ec76f0423e2d3ac
Neem Serra <neem@Neems-MacBook-Air.local>
2015-01-25 14:35:55 -0600

    Initial Commit

---
+++ Aerie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Aerie
+//
+//  Created by Neem Serra on 1/25/15.
+//  Copyright (c) 2015 TEAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Aerie/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Aerie
+//
+//  Created by Neem Serra on 1/25/15.
+//  Copyright (c) 2015 TEAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
